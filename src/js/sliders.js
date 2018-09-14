011a3c79f126c0e765bd625fca7ac52a39a8064c
EuvenyEmelin <emelin.eug@ya.ru>
2018-09-15 02:32:34 +0300

    2section

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,35 +1,39 @@
 $(document).ready(function() {
   //постраничная прокрутка
-  $('#pagepiling').pagepiling({
-    menu: null,
-    direction: 'vertical',
-    verticalCentered: false,
-    sectionsColor: [],
-    anchors: [],
-    scrollingSpeed: 20,
-    easing: 'swing',
-    loopBottom: false,
-    loopTop: false,
-    css3: true,
-    navigation: {
-      'textColor': '#000',
-      'bulletsColor': '#fff',
-      'position': 'right',
-      'tooltips': ['section1', 'section2', 'section3', 'section4']
-    },
-    normalScrollElements: null,
-    normalScrollElementTouchThreshold: 5,
-    touchSensitivity: 5,
-    keyboardScrolling: true,
-    sectionSelector: '.section',
-    animateAnchor: false,
+  // $('#pagepiling').pagepiling({
+  //   menu: null,
+  //   direction: 'vertical',
+  //   verticalCentered: false,
+  //   sectionsColor: [],
+  //   anchors: [],
+  //   scrollingSpeed: 20,
+  //   easing: 'easeInOutCubic',
+  //   loopBottom: false,
+  //   loopTop: false,
+  //   css3: true,
+  //   navigation: {
+  //     'textColor': '#000',
+  //     'bulletsColor': '#fff',
+  //     'position': 'right',
+  //     'tooltips': ['section1', 'section2', 'section3', 'section4']
+  //   },
+  //   normalScrollElements: null,
+  //   normalScrollElementTouchThreshold: 5,
+  //   touchSensitivity: 5,
+  //   keyboardScrolling: true,
+  //   sectionSelector: '.section',
+  //   animateAnchor: false,
+  //
+  //   //events
+  //   onLeave: function(index, nextIndex, direction){},
+  //   afterLoad: function(anchorLink, index){},
+  //   afterRender: function(){},
+  // })
 
-    //events
-    onLeave: function(index, nextIndex, direction){},
-    afterLoad: function(anchorLink, index){},
-    afterRender: function(){},
+  $.scrollify({
+    section: '.section',
+    scrollSpeed: 1100,
   })
-
 
   //основной слайдер
   new Swiper('.main-slider__container', {
@@ -39,13 +43,25 @@ $(document).ready(function() {
       crossFade: true
     },
     pagination: {
-      el: '.swiper-pagination'
+      el: '.main-slider__pagination'
     }
   })
 
   //карусель под слайдером
-  new Swiper('.main-carousel__container', {
+  let mainCarousel = new Swiper('.main-carousel__container', {
     slidesPerView: 5,
     spaceBetween: 5,
+  })
+  //календарь событий
+  let eventsCarousel = new Swiper('.events-container', {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    pagination: {
+      el: '.events-pagination',
+      bulletElement: 'div',
+      clickable: true,
+      bulletClass: 'pagination-bullets',
+      bulletActiveClass: 'pagination-bullets-active'
+    }
   })
 })
